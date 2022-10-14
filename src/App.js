f89d52b43a4ff53a6e116da6864e3b6ad388698a
kareem nour <kimzcodes@gmail.com>
2022-10-14 14:18:57 -0700

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Table from "react-bootstrap/Table";
+
+import Header from "./components/Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header />
+      <Row>
+        <Col xs={{ span: 8, offset: 2 }}>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th style={{ width: "10%" }}>#</th>
+                <th>Title</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td></td>
+              </tr>
+            </tbody>
+          </Table>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>CRUD</h1>
+      <ul className="nav">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/">Add Post</a>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default Header;

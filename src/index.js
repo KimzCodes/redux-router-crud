fef5d784f2c738dd415db849f148828e3336739d
kareem nour <kimzcodes@gmail.com>
2022-10-14 19:10:11 -0700

    bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
